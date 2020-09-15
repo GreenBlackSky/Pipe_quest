05481b1a29b442cb2580160eb969c8e820e32f92
GreenBlackSky <greenblacksky@gmail.com>
2020-09-16 00:33:47 +0300

    new sky and quests still

--- Assets/Scripts/Quest.cs
+++ Assets/Scripts/Quest.cs
@@ -1,12 +1,22 @@
 ﻿
 
 [System.Serializable]
-public class Quest
-{
-    // TODO create and implement quest nodes
+public class Quest {
     public string name;
     public string description;
 
-    public  string type;
+    public QuestNode head;
+}
+
+[System.Serializable]
+public class QuestNode {
+    public string description;
+    public string type;
+    public int targetItem;
     public int targetAmount;
+    public int progress = 0;
+
+    // TODO init more nodes (after json)
+    // TODO reward
+    public QuestNode nextNode;
 }
--- Assets/Scripts/QuestDoingHero.cs
+++ Assets/Scripts/QuestDoingHero.cs
@@ -1,31 +1,68 @@
 ﻿using System.Collections.Generic;
+using UnityEngine.Events;
 using UnityEngine;
 
-public class QuestDoingHero : MonoBehaviour, InteractionListener
-{
-    // TODO use structs to track quest progress
-    HashSet<Quest> _activeQuests;
-
+public class QuestDoingHero : MonoBehaviour, InteractionListener {
+    // TODO pass description and name to ui
     public GameObject QuestsUI;
-    // TODO unuse InteractionListener and chain a start of a quest to the start of a dialog
-    public void interact(GameObject interactable)
-    {
+    Dictionary<string, HashSet<QuestNode>> _activeQuests;
+
+    Dictionary<string, UnityAction> _callbacks;
+
+    // TODO unuse InteractionListener and chain a start of a quest to the dialog
+    public void interact(GameObject interactable) {
         QuestGiver questGiver = interactable.GetComponent<QuestGiver>();
         if (questGiver is null)
         {
             return;
         }
-        _activeQuests.Add(questGiver.quest);
-        EventManager.StartListening(questGiver.quest.type, ConditionMet);
+
+        QuestNode quest = questGiver.quest.head;
+        if(_activeQuests[quest.type].Count == 0) {
+            EventManager.StartListening(quest.type, _callbacks[quest.type]);
+        }
+        _activeQuests[quest.type].Add(quest);
     }
 
-    void ConditionMet()
-    {
-        Debug.Log("Completed");
+    void Callback(string Action) {
+        List<QuestNode> CompletedQuests = new List<QuestNode>();
+        List<QuestNode> NewQuests = new List<QuestNode>();
+        foreach(QuestNode quest in _activeQuests[Action]) {
+            quest.progress++;
+            if(quest.progress == quest.targetAmount) {
+                CompletedQuests.Add(quest);
+                if(!(quest.nextNode is null)) {
+                    NewQuests.Add(quest.nextNode);
+                }
+            }
+        }
+
+        foreach(QuestNode quest in NewQuests) {
+            if(_activeQuests[quest.type].Count == 0) {
+                EventManager.StartListening(quest.type, _callbacks[quest.type]);
+            }
+            _activeQuests[quest.type].Add(quest);
+        }
+
+        foreach(QuestNode quest in CompletedQuests) {
+            _activeQuests[Action].Remove(quest);
+        }
+        if(_activeQuests[Action].Count == 0) {
+            EventManager.StopListening(Action, _callbacks[Action]);
+        }
     }
 
-    void Start()
-    {
-        _activeQuests = new HashSet<Quest>();
+    void Collected() {
+        Callback("Collect");
+    }
+
+    // TODO new reach event
+    void Start() {
+        _callbacks = new Dictionary<string, UnityAction>() {
+            {"Collect", Collected}
+        };
+        _activeQuests = new Dictionary<string, HashSet<QuestNode>>() {
+            {"Collect", new HashSet<QuestNode>()}
+        };
     }
 }
